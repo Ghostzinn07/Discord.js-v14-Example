b70dc662d6e070611da53cb8e1e6cb7664b10dac
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-11-13 16:06:39 -0300

    📚 Modificando a index

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+
+global.client = new Client({ 
+     intents: [ 
+         GatewayIntentBits.Guilds, 
+         GatewayIntentBits.GuildMembers, 
+         GatewayIntentBits.GuildMessages, 
+         GatewayIntentBits.GuildVoiceStates, 
+         GatewayIntentBits.MessageContent 
+     ], 
+    disableMentions: 'everyone', 
+ }); 
+ 
+ client.login('token'); //Altere para o token da sua aplicação
