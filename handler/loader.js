05c68982a46393a20ccf9d3b983df65813fa0a50
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-11-13 21:13:29 -0300

    📚 Loader

--- handler/loader.js
+++ handler/loader.js
@@ -3,9 +3,19 @@ const { Collection } = require('discord.js');
 client.commands = new Collection(); 
  Array = []; 
 
+
+const eventos  = readdirSync('./eventos/').filter(file => file.endsWith('.js')); 
+
+ for (const file of eventos) { 
+     const evento = require(`../eventos/${file}`); 
+     console.log(`[ ${file.split('.')[0]} ]`); 
+     client.on(file.split('.')[0], evento.bind(null, client)); 
+     delete require.cache[require.resolve(`../eventos/${file}`)]; 
+ }; 
+
  readdirSync('./comandos/').forEach(dirs => { 
      const comandos = readdirSync(`./comandos/${dirs}`).filter(files => files.endsWith('.js')); 
-  
+ 
      for (const file of comandos) { 
          const comando = require(`../comandos/${dirs}/${file}`); 
          if (comando.name && comando.description) { 
