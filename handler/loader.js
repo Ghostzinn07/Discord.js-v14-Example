00820710cda21a04450371f0025b0e966b888976
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-11-13 21:03:34 -0300

    📚 Loader

--- handler/loader.js
+++ handler/loader.js
@@ -4,16 +4,16 @@ client.commands = new Collection();
  Array = []; 
 
  readdirSync('./comandos/').forEach(dirs => { 
-     const commands = readdirSync(`./comandos/${dirs}`).filter(files => files.endsWith('.js')); 
+     const comandos = readdirSync(`./comandos/${dirs}`).filter(files => files.endsWith('.js')); 
   
-     for (const file of commands) { 
-         const command = require(`../comandos/${dirs}/${file}`); 
-         if (command.name && command.description) { 
-         Array.push(command); 
-         console.log(`[ ${command.name.toLowerCase()} ]`); 
-         client.commands.set(command.name.toLowerCase(), command); 
-         delete require.cache[require.resolve(`../commands/${dirs}/${file}`)]; 
-         } else console.log(`Error ao carregar: [ ${command.name.toLowerCase()} ]`) 
+     for (const file of comandos) { 
+         const comando = require(`../comandos/${dirs}/${file}`); 
+         if (comando.name && comando.description) { 
+         Array.push(comando); 
+         console.log(`[ ${comando.name.toLowerCase()} ]`); 
+         client.commands.set(comando.name.toLowerCase(), comando); 
+         delete require.cache[require.resolve(`../comandos/${dirs}/${file}`)]; 
+         } else console.log(`Error ao carregar: [ ${comando.name.toLowerCase()} ]`) 
      }; 
  }); 
  client.on('ready', (client) => { 
