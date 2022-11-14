de170fb8e11453fab598cba472d63c6917114197
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-11-13 21:08:20 -0300

    📚 Ping

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,1 +1,8 @@
-
+module.exports = { 
+     name: 'ping', 
+     description: "Comando de ping", 
+     async execute({ client, i }) { 
+         i.reply(`Pong! ${client.ws.ping}ms`) 
+  
+     }, 
+ };
